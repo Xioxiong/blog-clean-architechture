9ff44778ec355ae8fed264637ba9d0a7e874bd56
Songbai Yan <colorguitar@hotmail.com>
2020-08-29 09:42:35 +0800

    first commit

---
+++ src/main/java/com/woody/ddd/blogcleanarchitechture/BlogCleanArchitechtureApplication.java
@@ -0,0 +1,13 @@
+package com.woody.ddd.blogcleanarchitechture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCleanArchitechtureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCleanArchitechtureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woody/ddd/blogcleanarchitechture/BlogCleanArchitechtureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woody.ddd.blogcleanarchitechture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCleanArchitechtureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
